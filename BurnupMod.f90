b1ad06fbec5210771bd129b48d36ff736fd128ef
Joshua Rady <joshuarady@Steppin-Razor.local>
2023-11-20 18:12:15 -0500

    Narrow in on intializations in Simulate().

--- BurnupMod.f90
+++ BurnupMod.f90
@@ -472,13 +472,14 @@ contains
 ! 		alone = 0.0
 ! 		area = 0.0
 ! 		fint = 0.0
-		! Not sure:
+		! Cleared:
 ! 		tdry = 0.0
 ! 		wodot = 0.0
 ! 		ddot = 0.0
 ! 		qcum = 0.0
-		tcum = 0.0
-		acum = 0.0
+		! In these 4:
+! 		tcum = 0.0
+! 		acum = 0.0
 		qdot = 0.0
 		key = 0
 		
